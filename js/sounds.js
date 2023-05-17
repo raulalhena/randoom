d5fa13e55dc37c35a1714c5878183ca480bd12ce
Georgina <georginatomass@gmail.com>
2023-05-17 18:35:36 +0200

    pequeños cambios que se pueden eliminar

--- js/sounds.js
+++ js/sounds.js
@@ -1,8 +1,13 @@
 
-// const sonidoseleccionado = new Audio("./sounds/winfantasia-6912.mp3");
 
-const reproducirSonidoMezclar = () => {
-    const sonido = new Audio("./sounds/cards-shuffling-87543.mp3");
+let mezclar_btn = document.getElementById("mezclar_btn")
+let empezar_nuevo_btn = document.getElementById("empezar_nuevo_btn")
+
+mezclar_btn.addEventListener("click", () => {
+    const sonido = new Audio("./sounds/riffle-card-shuffle-104313.mp3");
     sonido.play();
-
-}
+})
+empezar_nuevo_btn.addEventListener("click", () => {
+    const sonido = new Audio("./sounds/get_ready_1-104602.mp3");
+    sonido.play();
+})
