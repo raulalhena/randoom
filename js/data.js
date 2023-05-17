0df0d045cd2ccecaa832017e0526d856775c7e3a
Raul <raul.alhena@gmail.com>
2023-05-17 20:49:56 +0200

    shuffle button operative

--- js/data.js
+++ js/data.js
@@ -39,4 +39,89 @@ export const adventures = [
         selected: true,
         completed: true
     },
+    {
+        id: generarId(),
+        name: "Viajar a la Isla Sentinel",
+        description: "Isla ubicada en las islas Andamán, dónde vive una de las tirbus más aisladas y peligrosas del mundo",
+        category: "viajes",
+        selected: false,
+        completed: false
+    },
+    {
+        id: generarId(),
+        name: "Yougurt",
+        description: "Aventura en la región de Monsteny (a 30min de Barcelona), podréis descubrir desde los aires panoramas y paisajes impresionantes",
+        category: "comida",
+        selected: true,
+        completed: true
+    }, {
+        id: generarId(),
+        name: "Viajar a la Isla Sentinel",
+        description: "Isla ubicada en las islas Andamán, dónde vive una de las tirbus más aisladas y peligrosas del mundo",
+        category: "adrenalina",
+        selected: false,
+        completed: false
+    },
+    {
+        id: generarId(),
+        name: "Vuelo en globo aerostático",
+        description: "Aventura en la región de Monsteny (a 30min de Barcelona), podréis descubrir desde los aires panoramas y paisajes impresionantes",
+        category: "viajes",
+        selected: true,
+        completed: true
+    },
+    {
+        id: generarId(),
+        name: "Viajar a la Isla Sentinel",
+        description: "Isla ubicada en las islas Andamán, dónde vive una de las tirbus más aisladas y peligrosas del mundo",
+        category: "viajes",
+        selected: false,
+        completed: false
+    },
+    {
+        id: generarId(),
+        name: "Sopa de caracol",
+        description: "Aventura en la región de Monsteny (a 30min de Barcelona), podréis descubrir desde los aires panoramas y paisajes impresionantes",
+        category: "comida",
+        selected: true,
+        completed: true
+    }, {
+        id: generarId(),
+        name: "Viajar a la Isla Sentinel",
+        description: "Isla ubicada en las islas Andamán, dónde vive una de las tirbus más aisladas y peligrosas del mundo",
+        category: "adrenalina",
+        selected: false,
+        completed: false
+    },
+    {
+        id: generarId(),
+        name: "Vuelo en globo aerostático",
+        description: "Aventura en la región de Monsteny (a 30min de Barcelona), podréis descubrir desde los aires panoramas y paisajes impresionantes",
+        category: "viajes",
+        selected: true,
+        completed: true
+    },
+    {
+        id: generarId(),
+        name: "Mochila",
+        description: "Aventura en la región de Monsteny (a 30min de Barcelona), podréis descubrir desde los aires panoramas y paisajes impresionantes",
+        category: "viajes",
+        selected: true,
+        completed: true
+    }, {
+        id: generarId(),
+        name: "Nadar",
+        description: "Isla ubicada en las islas Andamán, dónde vive una de las tirbus más aisladas y peligrosas del mundo",
+        category: "adrenalina",
+        selected: false,
+        completed: false
+    },
+    {
+        id: generarId(),
+        name: "Antartida",
+        description: "Aventura en la región de Monsteny (a 30min de Barcelona), podréis descubrir desde los aires panoramas y paisajes impresionantes",
+        category: "viajes",
+        selected: true,
+        completed: true
+    },
 ]
--- js/showCards.js
+++ js/showCards.js
@@ -7,30 +7,33 @@ import { checkCategory } from "./checkCategory.js";
 
 // Obtenemos el elemento con la clase .card-container
 const cardContainer = document.querySelector(".card-container");
-const shuffledAdventures = shuffle(adventures);
 
+// Elimina todas los elementos hijos de un elemento html
+const removeAllChilds = (parent) => {
+    parent.querySelectorAll("article").forEach(child => {
+        child.remove()
+    });
+}
 
 const showCards = (userAdventures) => {
+    removeAllChilds(cardContainer);
 
     // Crea 12 elementos article dentro del elemento card-container
     for (let i = 0; i < 12; i++) {
         // alert(userAdventures[i].name)
         const child = cardContainer.appendChild(document.createElement("article"));
         child.classList.add(`card-front`, `card_${userAdventures[i].category}_img`);
-        child.innerHTML = `<h3>${userAdventures[i].name}</h3><a href="#" id="edit-btn"><img src="/img/edit.png" alt="Modificar aventura" /></a>`;
-
-        // `<img src=\"/img/${userAdventures[i].category}.jpg\" style=\"width: 55px; height: 100px;\" alt=\"${userAdventures[i].name}\" />`;
+        child.innerHTML = `<h3>${userAdventures[i].name}</h3><a href='#' id='edit-btn'><img src='/img/edit.png' alt='Modificar aventura' /></a>`;
     }
 
 }
 
-window.addEventListener("load", showCards(selectCategory(shuffledAdventures, checkCategory())));
+window.addEventListener("load", showCards(selectCategory(shuffle(adventures), checkCategory())));
 
 const shuffleBtn = document.getElementById("mezclar_btn");
 
 shuffleBtn.addEventListener("click", () => {
-    alert("ei")
-    showCards(selectCategory(shuffledAdventures, checkCategory()))
+    showCards(selectCategory(shuffle(adventures), checkCategory()))
 });
 
 
