302fe2dc598328c38715a9a7fe43d3a2c303f48d
andreubltzr <andreubltzr11@gmail.com>
2023-05-19 15:33:38 +0200

    updates

--- js/flipCards.js
+++ js/flipCards.js
@@ -1,9 +1,9 @@
 export const flipCards = (parent) => {
-    parent.querySelectorAll("article").forEach((child) => {
-        child.classList.toggle(`card-front`);
-        child.classList.toggle(`card-back`);
-        child.querySelector("img").classList.toggle("front-img");
-        child.querySelector("img").classList.toggle("back-img");
-        child.querySelector("img").src = "/img/todas.png";
-    });
+  parent.querySelectorAll("article").forEach((child) => {
+    child.classList.toggle(`card-front`);
+    child.classList.toggle(`card-back`);
+    child.querySelector("img").classList.toggle("front-img");
+    child.querySelector("img").classList.toggle("back-img");
+    child.querySelector("img").src = "/img/todas2.png";
+  });
 };
--- js/showCards.js
+++ js/showCards.js
@@ -4,13 +4,13 @@ const cardContainer = document.querySelector(".card-container");
 const customizeAdventureDialog = document.getElementById("customize-adventure");
 
 export const showCards = (userAdventures) => {
-    // Crea 12 elementos article dentro del elemento card-container
-    for (let i = 0; i < 12; i++) {
-        const child = cardContainer.appendChild(document.createElement("article"));
-        child.classList.add(`card-front`);
-        child.classList.add(`card_${userAdventures[i].category}_img`);
-        child.innerHTML = `
-        <img class="front-img" src='/img/${userAdventures[i].category}.jpg' alt='Modificar aventura' width="100%" height="100%" />
+  // Crea 12 elementos article dentro del elemento card-container
+  for (let i = 0; i < 12; i++) {
+    const child = cardContainer.appendChild(document.createElement("article"));
+    child.classList.add(`card-front`);
+    child.classList.add(`card_${userAdventures[i].category}_img`);
+    child.innerHTML = `
+        <img class="front-img" src='/img/${userAdventures[i].category}.jpg' alt='Modificar aventura'/>
 
         <div class="card-content-title">
             <h3>${userAdventures[i].name}</h5>
@@ -22,15 +22,10 @@ export const showCards = (userAdventures) => {
         </div>
         `;
 
-        const btn = document
-            .getElementById(`edit-btn-${userAdventures[i].id}`)
-            .addEventListener("click", () => {
-                customizeAdventureDialog.showModal();
-            });
-    }
+    const btn = document
+      .getElementById(`edit-btn-${userAdventures[i].id}`)
+      .addEventListener("click", () => {
+        customizeAdventureDialog.showModal();
+      });
+  }
 };
-
-
-
-
-
