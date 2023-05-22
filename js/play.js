6fd57e6dc7f0c326e24cc4edd17d79f5c99fb53e
EvangeRodriguez <eva.irodriguez93@gmail.com>
2023-05-22 23:15:00 +0200

    animación de las cartas al girar

--- js/play.js
+++ js/play.js
@@ -3,11 +3,14 @@ import { shuffle } from "./shuffle.js";
 import { addClickToCards } from "./addClickToCards.js";
 import { createCards } from "./createCards.js";
 import { removeAllChilds } from "./removeAllChilds.js";
+import { rotateElement } from "./animation.js";
 
 export const play = (cardContainer, userAdventures) => {
-    flipCards(cardContainer);
-    const shuffledAdventures = shuffle(userAdventures);
-    removeAllChilds(cardContainer);
-    createCards(cardContainer, shuffledAdventures, "back");
-    addClickToCards(cardContainer, shuffledAdventures);
-}
+  const shuffledAdventures = shuffle(userAdventures);
+  removeAllChilds(cardContainer);
+  createCards(cardContainer, shuffledAdventures, "back");
+  addClickToCards(cardContainer, shuffledAdventures);
+
+  // Aplicar animación de rotación a cada artículo
+  flipCards(cardContainer);
+};
